7c71bf7df0356b48030bafe7bbcc8e992268b070
vpetridis <vpetridis@modulus.gr>
2021-09-19 01:02:20 +0300

    INIT axios reqs

--- pages/books-list.js
+++ pages/books-list.js
@@ -1,10 +1,10 @@
 import React from "react"
+import axios from "axios"
 import { useRouter } from "next/router"
-
-const booksList = () => {
+const booksList = ({ posts }) => {
   const router = useRouter()
   // const { pid } = router.query
-  console.log(router.asPath)
+  console.log(posts)
   return (
     <div className="title">
       This is the Books List
@@ -19,6 +19,33 @@ const booksList = () => {
       </style>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const options = {
+    method: "GET",
+    url: "https://amazon-product-reviews-keywords.p.rapidapi.com/product/details",
+    params: { asin: "B07ZPKR714", country: "US" },
+    headers: {
+      "x-rapidapi-host":
+        "amazon-product-reviews-keywords.p.rapidapi.com",
+      "x-rapidapi-key":
+        "bc550237e8mshccad21bced7bf8bp11f7a2jsn68566ce37265",
+    },
+  }
+
+  try {
+    const res = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    )
+    const posts =  res.json()
+    return {
+      props: { posts },
+    }
+  } catch (error) {
+    console.error(error)
+    return new Error("didn't get anything")
+  }
 }
 
 export default booksList
