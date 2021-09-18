eddefab82904c2a07d24eac8d35d022b7e5a8b40
vpetridis <vpetridis@modulus.gr>
2021-09-18 23:25:56 +0300

    ADDED more meta tags to about page

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,17 @@
 import React from "react"
+import Head from "next/Head"
 import styles from "../styles/About.module.css"
 const about = () => {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>About</title>
+        <meta
+          name="Bookstore NextJs"
+          content="programming, nextjs, Bookstore"
+          description="trying to learn some stuff with nextJs"
+        />
+      </Head>
       <h1>about page</h1>
     </div>
   )
