642c0ba65918febebdff13d1295bf8d348935342
vpetridis <vpetridis@modulus.gr>
2021-09-18 23:17:57 +0300

    INIT About page w/ module.css

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import styles from "../styles/About.module.css"
+const about = () => {
+  return (
+    <div className={styles.container}>
+      <h1>about page</h1>
+    </div>
+  )
+}
+
+export default about
